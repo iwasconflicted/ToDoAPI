371b3f372f0f9d25eccb73b4b15015ca835b4ca3
Saroeut Sim <saroeuts123@gmail.com>
2024-04-30 20:59:07 -0700

    first commit

---
+++ api/Models/ToDoList.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class ToDoList
+    {
+        public int Id {get; set;}
+        public string? TaskOne {get; set;}
+        public string? TaskTwo {get; set;}
+        public string? TaskThree {get; set;}
+        public string? TaskFour {get; set;}
+
+    }
+}
