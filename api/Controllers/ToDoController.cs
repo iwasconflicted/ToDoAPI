89e5dec0d37d7736da05385c918b8e112cec2900
Saroeut Sim <saroeuts123@gmail.com>
2024-05-01 20:58:09 -0700

    put and get created

--- api/Controllers/ToDoController.cs
+++ api/Controllers/ToDoController.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -18,5 +21,42 @@ namespace api.Controllers
         _context = context;
       }
       
+      [HttpGet]
+
+      public async Task<IEnumerable<ToDoList>> getToDo()
+      {
+        var doList = await _context.ToDoLists.AsNoTracking().ToListAsync();
+        return doList;
+      }
+
+      [HttpPost]
+
+      public async Task<IActionResult> Create(ToDoList doList)
+      {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        await _context.AddAsync(doList);
+
+        var result = await _context.SaveChangesAsync();
+        if(result > 0)
+        {
+            return Ok();
+        }
+        return BadRequest();
+      }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
